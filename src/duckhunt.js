03fe83173523f4197272d6adcaa0ad319cbb5ab3
Matthias Meusburger <matthias.meusburger@gmail.com>
2020-04-28 18:52:14 +0200

    0.0.3: Various fixes
     - Correct use of setInterval
     - Use "room" everywhere

--- src/duckhunt.js
+++ src/duckhunt.js
@@ -24,20 +24,18 @@ module.exports = (robot) => {
     var maxducks = process.env.HUBOT_DUCKHUNT_DUCKS || 5;
 
     robot.respond(/starthunt/gi, (res) => {
-        currentroom = res.message.room;
-        if (Array.isArray(games[currentroom])) {
+        room = res.message.room;
+        if (Array.isArray(games[room])) {
             res.send("There is already a hunt!");
         } else {
             res.send("The hunt starts now!");
-            games[currentroom] = new Array();
-            games[currentroom]['scores'] = new Array();
-            games[currentroom]['lastspoke'] = Date.now();
-            games[currentroom]['throttle'] =  Math.floor(Math.random() * Math.floor(maxthrottle));
-            games[currentroom]['duck'] =  false;
-            games[currentroom]['ducks'] = 0;
-            games[currentroom]['timer'] = setInterval(function() {
-                launch(currentroom);
-            }, 1000);
+            games[room] = new Array();
+            games[room]['scores'] = new Array();
+            games[room]['lastspoke'] = Date.now();
+            games[room]['throttle'] =  Math.floor(Math.random() * Math.floor(maxthrottle));
+            games[room]['duck'] =  false;
+            games[room]['ducks'] = 0;
+            games[room]['timer'] = setInterval(launch, 1000, room);
         }
     })
 
@@ -45,12 +43,12 @@ module.exports = (robot) => {
         stopHunt(res);
     })
 
-    function launch(currentroom) {
-        if (games[currentroom]['duck'] == false && Date.now() > games[currentroom]['lastspoke'] + games[currentroom]['throttle']) {
-            robot.messageRoom(currentroom, "\\_o< quack!");
-            games[currentroom]['duck'] = true;
-            games[currentroom]['ducks'] += 1;
-            games[currentroom]['lastspoke'] = Date.now();
+    function launch(room) {
+        if (games[room] && games[room]['duck'] == false && Date.now() > games[room]['lastspoke'] + games[room]['throttle']) {
+            robot.messageRoom(room, "\\_o< quack!");
+            games[room]['duck'] = true;
+            games[room]['ducks'] += 1;
+            games[room]['lastspoke'] = Date.now();
         }
     }
 
@@ -77,17 +75,17 @@ module.exports = (robot) => {
             res.reply("There is no hunt!");
             return;
         }
-        if (games[currentroom]['duck']) {
-            res.reply("\\_x< : +1 (" + ((Date.now() - games[currentroom]['lastspoke']) / 1000) + " seconds)");
-            games[currentroom]['scores'][user] = games[currentroom]['scores'] ? games[currentroom]['scores'][user] + 1 : 1;
-            games[currentroom]['duck'] = false;
-            games[currentroom]['throttle'] =  Math.floor(Math.random() * Math.floor(maxthrottle));
-            games[currentroom]['lastspoke'] = Date.now();
+        if (games[room]['duck']) {
+            res.reply("\\_x< : +1 (" + ((Date.now() - games[room]['lastspoke']) / 1000) + " seconds)");
+            games[room]['scores'][user] = games[room]['scores'][user] ? games[room]['scores'][user] + 1 : 1;
+            games[room]['duck'] = false;
+            games[room]['throttle'] =  Math.floor(Math.random() * Math.floor(maxthrottle));
+            games[room]['lastspoke'] = Date.now();
         } else {
             res.reply("There was no duck: -1");
-            games[currentroom]['scores'][user] = games[currentroom]['scores'][user] ? games[currentroom]['scores'][user] - 1 : -1;
+            games[room]['scores'][user] = games[room]['scores'][user] ? games[room]['scores'][user] - 1 : -1;
         }
-        if (games[currentroom]['ducks'] == maxducks) {
+        if (games[room]['ducks'] == maxducks) {
             stopHunt(res);
         }
     })
